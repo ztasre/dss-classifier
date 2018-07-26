ff34c038c243cf2d471e8db9e98d9300f3d072e5
ztasre <ztasre@devnull.com>
2018-07-25 20:42:23 -0400

    Transfer learning off of mnist-model.py

---
+++ mnist-transfer.py
@@ -0,0 +1,86 @@
+#!/bin/env python
+
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+ 
+train_dir = "data/dss-data-cat/train"
+test_dir = "data/dss-data-cat/test"
+
+mnist_conv_base = models.load_model('mnist-base.h5')
+
+"""
+Base mode for transfer learning is configured below. 
+1. Layers are shaved off.
+2. Layers are set to be none trainable
+"""
+
+mnist_conv_base.trainable = False
+
+train_data_gen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=180
+        )
+
+test_data_gen = ImageDataGenerator(
+        rescale=1./255
+        )
+
+train_generator = train_data_gen.flow_from_directory(
+        train_dir,
+        target_size=(28, 28),
+        batch_size=20,
+        color_mode="grayscale"
+        )
+
+test_generator = test_data_gen.flow_from_directory(
+        test_dir,
+        target_size=(28, 28),
+        batchsize=20,
+        color_mode="grayscale"
+        )
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(9, activation="softmax"))
+
+model.compile(loss="categorical_crossentroy",
+              optimizer=optimizers.Adadelta(),
+              metrics=['acc'])
+"""
+    Total amount of data in train: 8168
+
+    eight: 856
+    five: 845
+    four: 970
+    nine: 912
+    one: 741
+    seven: 960
+    six: 1197
+    three: 843
+    two: 844
+
+    Total amount of data in test: 91
+
+    eight: 10
+    five: 10
+    four: 10
+    nine: 11
+    one: 10
+    seven: 10
+    six: 10
+    three: 10
+    two: 10
+
+"""
+history = model.fit_generator(
+        train_generator,
+        steps_per_epocs=1337,
+        epochs=10,
+        validation_data=test_generator,
+        validation_steps=50)
+
